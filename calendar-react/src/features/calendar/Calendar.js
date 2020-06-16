042813c6792ab62c5db7674d1747ff5268f9437c
clarkj99 <clark@stontech.net>
2020-06-16 16:18:46 -0400

    add highlight for current day

--- calendar-react/src/features/calendar/Calendar.js
+++ calendar-react/src/features/calendar/Calendar.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import './Calendar.css'
-// import moment from 'moment'
 
 function adjustedMonth(month) {
     if (month < 0)
@@ -10,22 +9,10 @@ function adjustedMonth(month) {
     return month
 }
 
-function calendarDays(day) {
-    const days = []
-    day.date(1)
-    day.add(-(day.day() + 1), "d")
-    for (let i = 1; i <= 42; i++) {
-        day.add(1, "d")
-        days[i] = { day: day.date(), month: day.month() }
-    }
-    return days
-}
-
 function calendarDaysJS(day) {
     const days = []
     day.setDate(1)
     day.setDate(1 - day.getDay())
-    // day.add(-(day.day() + 1), "d")
     for (let i = 1; i <= 42; i++) {
         days[i] = { day: day.getDate(), month: day.getMonth() }
         day.setDate(day.getDate() + 1)
@@ -33,17 +20,41 @@ function calendarDaysJS(day) {
     return days
 }
 
+function dateClass(item, month, today) {
+    let dateClass = item.month === month ? "active-day" : "inactive-day"
+
+    if (item.day === today) {
+        dateClass += " current"
+    }
+    return dateClass
+}
+
+function eventModal() {
+    return (
+        <div id="myModal" class="modal-inactive">
+            <div class="modal-content">
+                <span class="close">&times;</span>
+                <p>Some text in the Modal..</p>
+            </div>
+
+        </div>
+    )
+}
+
+const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+
+const fullMonths = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+const days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+const fullDays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
 export function Calendar() {
-    const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
-    const fullMonths = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-    const days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
-    const fullDays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
     const today = new Date();
     const month = today.getMonth();
     const date = today.getDate();
     const weekday = today.getDay();
-
-    // const day = moment()
 
     return (
         <div className="section">
@@ -57,20 +68,20 @@ export function Calendar() {
                     <span>{months[today.getMonth()]}</span>
                     <span className="inactive-month">{months[adjustedMonth(today.getMonth() + 1)]}</span>
                 </div>
-                <div className="divider-line">
+                <div className="h-line">
                     <hr />
                 </div>
                 <div className="calendar-controls">
-                    <div className="calendar-arrow"> {String.fromCharCode(8592)} </div>
-                    <div>
+                    <div className="calendar-arrow"> {String.fromCharCode(10132)} </div>
+                    <div className="calendar-body">
                         <div className="calendar-days">
                             {days.map(day => <span key={day}>{day}</span>)}
                         </div>
                         <div className="calendar-dates">
-                            {calendarDaysJS(today).map(item => <span key={item.day} className={item.month == month ? "active-day" : "inactive-day"}>{item.day}</span>)}
+                            {calendarDaysJS(today).map(item => <span key={item.month + ' ' + item.day} className={dateClass(item, month, date)}>{item.day}</span>)}
                         </div>
                     </div>
-                    <div className="calendar-arrow">{String.fromCharCode(8594)}</div>
+                    <div className="calendar-arrow">{String.fromCharCode(10132)}</div>
                 </div>
             </div>
             <div className="event-box">
@@ -78,15 +89,16 @@ export function Calendar() {
                 <div className="event-date"><span>{fullDays[weekday]}</span> <span>{fullMonths[month]} {date}</span></div>
 
                 <div className="event-details">
-                    <div className="vl"></div>
+                    <div className="v-line"></div>
                     <div className="list">
                         <div>10:30 Goat Yoga</div>
                         <div>10:30 Goat Yoga</div>
                         <div>10:30 Goat Yoga</div>
                     </div>
                 </div>
-                <div className="event-create">+</div>
+                <button className="event-create">+</button>
             </div>
+            {eventModal()}
         </div>
     )
 }
