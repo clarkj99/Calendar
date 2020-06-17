f485c0fa82cf561766f7b967a1857d24c0b0e2a8
clarkj99 <clark@stontech.net>
2020-06-17 01:55:44 -0400

    functional month change

--- calendar-react/src/features/calendar/Calendar.js
+++ calendar-react/src/features/calendar/Calendar.js
@@ -11,22 +11,23 @@ function adjustedMonth(month) {
 
 function calendarDaysJS(day) {
     const days = []
-    day.setDate(1)
-    day.setDate(1 - day.getDay())
+    const tempDate = new Date(day.getTime())
+    tempDate.setDate(1)
+    tempDate.setDate(1 - tempDate.getDay())
     for (let i = 1; i <= 42; i++) {
-        days[i] = { day: day.getDate(), month: day.getMonth() }
-        day.setDate(day.getDate() + 1)
+        days[i] = { day: tempDate.getDate(), month: tempDate.getMonth(), year: tempDate.getYear() }
+        tempDate.setDate(tempDate.getDate() + 1)
     }
     return days
 }
 
 function dateClass(item, month, today) {
-    let dateClass = item.month === month ? "active-day" : "inactive-day"
+    let className = item.month === month ? "active-day" : "inactive-day"
 
-    if (item.day === today) {
-        dateClass += " current"
+    if (item.day === today.getDate() && item.month === today.getMonth() && item.yesr === today.getYear()) {
+        className += " current"
     }
-    return dateClass
+    return className
 }
 
 function eventModal(modalActive, setModalActive) {
@@ -45,11 +46,11 @@ function eventModal(modalActive, setModalActive) {
                         </div>
                         <div>
                             <label>Start Date: </label>
-                            <input type="text" name="title" />
+                            <input name="startdate" type="date" />
 
                         </div>
                         <div><label>Begins:  </label>
-                            <input type="text" name="begins" />
+                            <input name="begins" type="time" />
 
                         </div>
                         <div><label>People:</label>
@@ -77,8 +78,11 @@ const toggleModal = (modalActive, setModalActive) => {
     setModalActive(!modalActive)
 }
 
-const changeMonth = (month) => {
-    alert(month)
+const changeMonth = (number, dateForMonth, setDateForMonth) => {
+    // alert(number)
+    const newDate = new Date(dateForMonth.getTime())
+    newDate.setMonth(dateForMonth.getMonth() + number, 1)
+    setDateForMonth(newDate)
 }
 
 const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
@@ -90,42 +94,45 @@ const days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
 const fullDays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 
 export function Calendar() {
-    const [modalActive, setModalActive] = useState(true);
     const today = new Date();
-    const month = today.getMonth();
+
+    const [modalActive, setModalActive] = useState(false);
+    const [dateForMonth, setDateforMonth] = useState(new Date())
+
+    const month = dateForMonth.getMonth();
     const date = today.getDate();
-    const weekday = today.getDay();
+    const weekday = dateForMonth.getDay();
 
     return (
         <div className="section">
             <div className='calendar-box'>
                 <div className="calendar-header">
                     <span className="calendar-title">eCalendar</span>
-                    <span className="calendar-year">{today.getFullYear()}</span>
+                    <span className="calendar-year">{dateForMonth.getFullYear()}</span>
                 </div>
                 <div className="calendar-months">
-                    <span className="inactive-month">{months[adjustedMonth(today.getMonth() - 1)]}</span>
-                    <span>{months[today.getMonth()]}</span>
-                    <span className="inactive-month">{months[adjustedMonth(today.getMonth() + 1)]}</span>
+                    <span className="inactive-month">{months[adjustedMonth(dateForMonth.getMonth() - 1)]}</span>
+                    <span>{months[dateForMonth.getMonth()]}</span>
+                    <span className="inactive-month">{months[adjustedMonth(dateForMonth.getMonth() + 1)]}</span>
                 </div>
                 <div className="h-line">
                     <hr />
                 </div>
                 <div className="calendar-controls">
-                    <span className="calendar-arrow" onClick={() => changeMonth(0)}> {String.fromCharCode(10132)} </span>
+                    <span className="calendar-arrow text-button" onClick={() => changeMonth(-1, dateForMonth, setDateforMonth)}> {String.fromCharCode(10132)} </span>
                     <div className="calendar-body">
                         <div className="calendar-days">
                             {days.map(day => <span key={day}>{day}</span>)}
                         </div>
                         <div className="calendar-dates">
-                            {calendarDaysJS(today).map(item => <span key={item.month + ' ' + item.day} className={dateClass(item, month, date)}>{item.day}</span>)}
+                            {calendarDaysJS(dateForMonth).map(item => <span key={item.month + ' ' + item.day} className={dateClass(item, month, today)}>{item.day}</span>)}
                         </div>
                     </div>
-                    <span className="calendar-arrow" onClick={() => changeMonth(1)}>{String.fromCharCode(10132)}</span>
+                    <span className="calendar-arrow text-button" onClick={() => changeMonth(1, dateForMonth, setDateforMonth)}>{String.fromCharCode(10132)}</span>
                 </div>
             </div>
             <div className="event-box">
-                <div className="event-signin">Sign In</div>
+                <div className="event-signin text-button">Sign In</div>
                 <div className="event-date"><span>{fullDays[weekday]}</span> <span>{fullMonths[month]} {date}</span></div>
 
                 <div className="event-details">
@@ -136,7 +143,7 @@ export function Calendar() {
                         <div>10:30 Goat Yoga</div>
                     </div>
                 </div>
-                <div className="event-create" onClick={() => toggleModal(modalActive, setModalActive)}>+</div>
+                <div className="event-create text-button" onClick={() => toggleModal(modalActive, setModalActive)}>+</div>
             </div>
             {modalActive && eventModal(modalActive, setModalActive)}
         </div>
