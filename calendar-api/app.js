d4567b876e30687f090391778cbbe73911f98318
clarkj99 <clark@stontech.net>
2020-06-26 00:26:28 -0400

    Utilized useEffect for fetch on get /events

--- calendar-api/app.js
+++ calendar-api/app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors')
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
@@ -8,7 +9,7 @@ const eventsRouter = require('./routes/events');
 
 const app = express();
 const port = 9000;
-
+app.use(cors())
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
