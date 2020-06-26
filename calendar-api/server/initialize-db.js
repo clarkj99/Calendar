bcf19ae7d5fdb82873e1fc3a5235427d8d831221
clarkj99 <clark@stontech.net>
2020-06-25 23:02:27 -0400

    initialize database

---
+++ calendar-api/server/initialize-db.js
@@ -0,0 +1,14 @@
+const initialEvents = require('./initialEvents')
+const connectDB = require('./connect-db')
+
+async function initializeDB() {
+    const db = await connectDB()
+    for (let collectionName in initialEvents) {
+        let collection = db.collection(collectionName)
+        console.log(collectionName)
+        await collection.insertMany(initialEvents[collectionName])
+    }
+
+}
+
+initializeDB()
