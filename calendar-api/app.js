e365d7c718c1bdf6c16f79b62a6bd559df0e1ea1
clarkj99 <clark@stontech.net>
2020-07-04 23:24:08 -0400

    Use dotenv for environment vars

--- calendar-api/app.js
+++ calendar-api/app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const dotenv = require('dotenv').config();
 const cors = require('cors')
 const path = require('path');
 const cookieParser = require('cookie-parser');
